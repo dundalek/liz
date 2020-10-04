1fb85724d0a463e6db872bd4a81f0a8ba369a744
Jakub Dundalek <dundalek@gmail.com>
2020-10-04 13:00:56 +0200

    Fix source info with edamame reader

--- src/liz/impl/reader.clj
+++ src/liz/impl/reader.clj
@@ -1,18 +1,22 @@
 (ns liz.impl.reader
   (:require [edamame.core :as edamame]
-            [clojure.tools.reader :as reader]
-            [clojure.tools.reader.reader-types :as rt]))
+            #_[clojure.tools.reader :as reader]
+            #_[clojure.tools.reader.reader-types :as rt]))
 
-(defn read-all
-  ([rdr] (read-all {:eof (Object.)} rdr))
-  ([{:keys [eof] :as opts} rdr]
-   (let [form (reader/read opts rdr)]
-     (when-not (identical? form eof)
-       (cons form (lazy-seq (read-all opts rdr)))))))
-
-(defn read-all-string [s]
-  (read-all (rt/indexing-push-back-reader
-              (rt/string-reader s))))
+#_(defn read-all
+    ([rdr] (read-all {:eof (Object.)} rdr))
+    ([{:keys [eof] :as opts} rdr]
+     (let [form (reader/read opts rdr)]
+       (when-not (identical? form eof)
+         (cons form (lazy-seq (read-all opts rdr)))))))
 
 #_(defn read-all-string [s]
-    (edamame/parse-string-all s {:deref true}))
+    (read-all (rt/indexing-push-back-reader
+                (rt/string-reader s))))
+
+(defn read-all-string [s]
+  (edamame/parse-string-all s {:deref true
+                               :row-key :line
+                               :col-key :column
+                               :end-row-key :end-line
+                               :end-col-key :end-column}))
